1d2a5e116ad008b1c3fd193d20e2d32a9a92abc4
kauan-kyoshi <kauankyoshik@gmail.com>
2025-07-13 11:07:28 -0300

    init libft

---
+++ libft/srcs/ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyoshi <kyoshi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 21:43:04 by kyoshi            #+#    #+#             */
+/*   Updated: 2025/07/12 21:46:23 by kyoshi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	c;
+	int				diff;
+
+	c = 0;
+	diff = 0;
+	while ((c < n) && !diff && (s1[c] != '\0') && (s2[c] != '\0'))
+	{
+		diff = (unsigned char)s1[c] - (unsigned char)s2[c];
+		c++;
+	}
+	if (c < n && !diff && (s1[c] == '\0' || s2[c] == '\0'))
+		diff = (unsigned char)s1[c] - (unsigned char)s2[c];
+	return (diff);
+}
