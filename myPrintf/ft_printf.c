b447dc1554783fdd341a99e5d7e028ea09d9d510
Kauan Kyoshi Kubo Lima <kauankyoshik@gmail.com>
2025-08-01 14:25:57 -0300

    done

--- myPrintf/ft_printf.c
+++ myPrintf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kakubo-l <kakubo-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 14:32:57 by kakubo-l          #+#    #+#             */
-/*   Updated: 2025/08/01 13:37:50 by kakubo-l         ###   ########.fr       */
+/*   Updated: 2025/08/01 14:19:55 by kakubo-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ static	int	ft_format_check(const char flag, va_list args)
 		return (ft_print_unsigned(va_arg(args, unsigned int)));
 	else if (flag == 'x' || flag == 'X')
 		return (ft_print_hex(va_arg(args, unsigned int), flag));
+	else if (flag == 'p')
+		return (ft_print_pointer(va_arg(args, void *)));
 	else if (flag == '%')
 		return (ft_print_char('%'));
 	return (0);
