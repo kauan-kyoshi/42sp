0b0c57af6282c234c0475b6210cea344fe9881ad
Kauan Kyoshi Kubo Lima <kauankyoshik@gmail.com>
2025-08-05 14:37:34 -0300

    done

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,17 +6,12 @@
 /*   By: kakubo-l <kakubo-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 17:59:25 by kakubo-l          #+#    #+#             */
-/*   Updated: 2025/08/01 17:59:28 by kakubo-l         ###   ########.fr       */
+/*   Updated: 2025/08/05 14:36:12 by kakubo-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/**
- * @brief Calcula o comprimento de uma string.
- * @param s A string a ser medida.
- * @return O comprimento da string.
- */
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
@@ -29,12 +24,6 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-/**
- * @brief Procura pela primeira ocorrência de um caractere em uma string.
- * @param s A string onde a busca será feita.
- * @param c O caractere a ser procurado.
- * @return Um ponteiro para o caractere encontrado, ou NULL se não for encontrado.
- */
 char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
@@ -50,41 +39,41 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-/**
- * @brief Concatena duas strings, criando uma nova string.
- * Libera a memória da primeira string (s1).
- * @param s1 A primeira string (será liberada).
- * @param s2 A segunda string a ser anexada.
- * @return A nova string concatenada, ou NULL em caso de falha de alocação.
- */
-char	*ft_strjoin(char *s1, char *s2)
+static char	*ft_str_copy_cat(char *new_str, char *s1, char *s2)
 {
-	char	*new_str;
 	size_t	i;
 	size_t	j;
 
+	i = -1;
+	while (s1[++i])
+		new_str[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		new_str[i + j] = s2[j];
+	new_str[i + j] = '\0';
+	return (new_str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+
 	if (!s1)
 	{
-		s1 = (char *)malloc(1 * sizeof(char));
+		s1 = (char *)malloc(1);
 		if (!s1)
 			return (NULL);
-		s1[0] = '\0';
+		*s1 = '\0';
 	}
 	if (!s2)
 		return (s1);
-	new_str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	new_str = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!new_str)
 	{
 		free(s1);
 		return (NULL);
 	}
-	i = -1;
-	j = -1;
-	while (s1[++i])
-		new_str[i] = s1[i];
-	while (s2[++j])
-		new_str[i + j] = s2[j];
-	new_str[i + j] = '\0';
+	new_str = ft_str_copy_cat(new_str, s1, s2);
 	free(s1);
 	return (new_str);
 }
