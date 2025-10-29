b8b66259974d16b964a35d95222b70e568494cc2
Kauan Kyoshi Kubo Lima <kauankyoshik@gmail.com>
2025-10-29 11:05:33 -0300

    done

--- RUSH-1(IMERSAO_PYTHON)/test.py
+++ RUSH-1(IMERSAO_PYTHON)/test.py
@@ -1,16 +1,32 @@
+
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         input_name = sys.argv[1]
         
+        COLOR_GREEN = '\033[92m'
+        COLOR_RED = '\033[91m'
+        COLOR_END = '\033[0m'
+
+
         result = fit(input_name)
         length = len(result)
+
+        if length <= 26:
+            status_color = COLOR_GREEN
+            status_text = "OK - Parabéns!! =)"
+        else:
+            status_color = COLOR_RED
+            status_text = "KO - Tente Novamente =("
+
+        colored_status = f"{status_color}{status_text}{COLOR_END}"
+
         status = "OK" if length <= 26 else "FALHA (Excede o limite)"
         
         print(f"Original:  {input_name} (caracteres: {len(input_name)})")
         print(f"Resultado: {result} (caracteres: {length})")
-        print(f"Status:    {status} (Limite: 26)")
+        print(f"Status:    {colored_status} (Limite: 26)")
         print("------------------------------------------------")
         
     elif len(sys.argv) < 2:
