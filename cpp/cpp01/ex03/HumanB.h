0213ddf6c3eff73b3321866d9fbb05c0e4fc48f6
kauan-kyoshi <kauankyoshik@gmail.com>
2026-07-24 17:43:58 -0300

    ex03 done

--- cpp/cpp01/ex03/HumanB.h
+++
@@ -1,6 +0,0 @@
-#ifndef HUMANB_H
-#define HUMANB_H
-
-#include "HumanB.hpp"
-
-#endif
--- cpp/cpp01/ex03/main.cpp
+++ cpp/cpp01/ex03/main.cpp
@@ -3,20 +3,36 @@
 
 int main()
 {
-	{
-		Weapon club = Weapon("crude spiked club");
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
-		HumanB jim("Jim");
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
+
+	Weapon sword = Weapon("long sword");
+	HumanA humanA1("Human A1", sword);
+	humanA1.attack();
+	sword.setType("short sword");
+	humanA1.attack();
+
+	std::cout << std::endl;
+
+	Weapon bow = Weapon("recurve bow");
+	HumanB HumanB1("Human B1");
+	HumanB1.setWeapon(bow);
+	HumanB1.attack();
+	bow.setType("long bow");
+	HumanB1.attack();
+
+	std::cout << std::endl;
+
+	Weapon pistol = Weapon("glock");
+	HumanA humanA2("Human A2", pistol);
+	// humanA2.attack();
+	pistol.setType("p250");
+	// humanA2.attack();
+
+	Weapon ar = Weapon("ak-47");
+	HumanB HumanB2("Human B2");
+	// HumanB2.setWeapon(ar);
+	HumanB2.attack();
+	ar.setType("m4a1");
+	HumanB2.attack();
+
 	return 0;
 }
